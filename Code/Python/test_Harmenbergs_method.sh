a5244c393331378325b989b0d0f2d7bfd1403fce
llorracc <git@llorracc.org>
2022-01-05 14:07:36 -0500

    Fix paths with test_Harmenbers_method - Public update

--- Code/Python/test_Harmenbergs_method.sh
+++ Code/Python/test_Harmenbergs_method.sh
@@ -2,19 +2,18 @@
 
 # Install requirements for Harmenberg test, if not already installed
 if [ ! -e requirements_Harmenberg.out ]; then
-    pip freeze --user > requirements_user.txt # Store original setup
-    pip install --user -r requirements_Harmenberg.txt > requirements_Harmenberg.out
+#    pip freeze --user > requirements_user.txt # Store original setup
+    export PYTHONPATH=$(python -c "import site, os; print(os.path.join(site.USER_SITE,'lib','python','site-packages'))"):$PYTHONPATH:./HarKmenberg/
+    mkdir HarKmenberg
+    pip install --target HarKmenberg -r requirements_Harmenberg.txt | tee requirements_Harmenberg.out
 fi
 
 # Descend to the point in the HARK toolkit where the tests are executed
 pushd .
-cd src/econ-ark/HARK/ConsumptionSaving/tests
 
 # Execute only the test of Harmenberg's method
-pytest --log-cli-level=DEBUG test_IndShockConsumerType.py -k test_Harmenbergs_method
+python -m pytest --log-cli-level=DEBUG src/econ-ark/HARK/ConsumptionSaving/tests/test_IndShockConsumerType.py -k test_Harmenbergs_method
 
 # Return to original point
 popd 
 
-# Restore original setup
-pip install --user -r requirements_user.txt
